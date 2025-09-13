4fcc59788651a23fbee27f4d3b14765ab8230537
NicolasLeonardoo <nicolasleonardo001@gmail.com>
2025-09-13 12:22:02 -0300

    adiciona funcao type next

--- script/script.js
+++ script/script.js
@@ -1,13 +1,26 @@
-let nextDom = document.getElementById("next");
-let prevDom = document.getElementById("prev");
-let carrosselDom = document.querySelector(".carrossel");
-let listaItemDom = document.querySelector(".carrossel .lista");
-let thumbnailDom = document.querySelector(".carrossel .thumbnail");
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+let carrosselDom = document.querySelector('.carrossel');
+let listaItemDom = document.querySelector('.carrossel .lista');
+let thumbnailDom = document.querySelector('.carrossel .thumbnail');
 
-nextDom.onclick = function() {
+nextDom.onclick = function () {
     showSlider('next');
 }
-function showSlider(type) { 
-    let itemSlider = document.querySelectorAll(".carrossel .lista .item");
-    let itemThumbnial = document.querySelectorAll(".carrossel .thumbnail .item");
+let timeRunning = 3000;
+let runTimeOut;
+function showSlider(type) {
+    let itemSlider = document.querySelectorAll('.carrossel .lista .item');
+    let itemThumbnail = document.querySelectorAll('.carrossel .thumbnail .item');
+
+    if (type === 'next') {
+        listaItemDom.appendChild(itemSlider[0]);
+        thumbnailDom.appendChild(itemThumbnail[0]);
+        carrosselDom.classList.add('next');
+    }
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() => {
+        carrosselDom.classList.remove('next');
+
+    }, timeRunning);
 }
